cbbc6cfc55a5776b5ea1c13c51d1e4850aadeb1e
PrestonWalker06 <pwalk2006@gmail.com>
2026-01-17 02:46:33 +0000

    Lab 2 Done

--- lab1_4.py
+++ lab1_4.py
@@ -1,1 +1,15 @@
-pass
+def calculate_average (num1, num2, num3):
+    return (num1 + num2 + num3) / 3
+num1 = 3
+num2 = 4
+num3 = 5
+average1 = calculate_average (num1, num2, num3)
+
+def add_tax (bill_total):
+    return (bill_total * 1.1)
+bill_total = 40
+bill_with_tax = add_tax (bill_total)
+
+def greet_user (name):
+    return (f"Hello {name}")
+greeting_Alice = greet_user ("Alice")
--- lab1_5.py
+++ lab1_5.py
@@ -1,1 +1,20 @@
-pass
+def check_multiple (number):
+    if number % 3 == 0 and number % 5 == 0:
+        return True
+    else:
+        return False
+
+def check_password (input_string):
+    if input_string == "Python123":
+        return "access granted"
+    else: 
+        return "access denied"
+    
+def calculate_federal_tax (salary):
+    if salary <= 11000:
+        return (1000.0)
+    elif salary <= 50000:
+        return (11000.0)
+    else:
+        return (24000.0)
+        
